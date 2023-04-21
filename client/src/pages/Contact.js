4c176d563066e75c562b029912d8495dc2262603
Jose Garcia <jose@Joses-MacBook-Air.local>
2023-04-21 16:21:21 -0400

    update contact page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
-import { validateEmail } from '../helpers/helper';
-import "../styles/Contact.css";
+import { validateEmail } from '../utils/helpers';
+import "../styles/contact.css";
 
 function Contact() {
 
@@ -34,3 +34,41 @@ function Contact() {
       function handleSubmit(event) {
         event.preventDefault();
     }
+    return (
+       
+        <div class="flex-column justify-content-center" id='contactDiv'>
+          <h2 className='titleNavsPage'>Contact</h2> 
+          <p><b><i>Please contact us!</i></b></p>
+
+          <form class=" flex-column justify-content-center" id="contact-form"   >
+              <div>
+                  <label htmlFor="name">Name:</label>
+                  <input class="form-control" type="text" name="name"  defaultValue={name} onBlur={handleChange}/>
+              </div>
+              <div >
+                  <label htmlFor="email">Email:</label>
+                  <input class="form-control" type="email"  name="email" defaultValue={email} onBlur={handleChange} />
+              </div>
+              <div>
+                  <label htmlFor="message">Message:</label>
+                  <textarea class="form-control" name="message" defaultValue={message} onBlur={handleChange} rows="7" />
+              </div> 
+              <div/>
+              {error && (
+              <div>
+                  <p className="error-text">{error}</p>
+              </div>
+              )}
+  
+              <div>
+              <button id='button' data-testid='button' class="btn btn-outline-dark mt-4" type="submit" onSubmit={handleSubmit}>Submit</button>
+              </div>
+              <br></br>
+          </form>
+          <br></br>
+        </div>  
+
+        );
+  
+  }
+export default Contact;
