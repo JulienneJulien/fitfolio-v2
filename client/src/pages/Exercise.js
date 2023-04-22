c17635341ea788b66828d28ecfb41fce418a9f23
Your Name <JulienneJulien15@gmail.com>
2023-04-21 21:22:19 -0400

    Added heroBanner vid and initial styling

--- client/src/pages/Exercise.js
+++ client/src/pages/Exercise.js
@@ -1,46 +1,17 @@
-import { useQuery } from '@apollo/client'
-// import {GET_ALL_POSTS} from '../utils/queries';
-// ADDED SPINNER FOR PAGE LOAD
-import spinner from '../assets/spinner.gif';
 import React from 'react'
-
+import { Segment, Image, Header} from 'semantic-ui-react'
+import video from '../assets/welcomeVid.mp4'
+import '../styles/Exercise.css'
 function Exercise() {
-    // const {data,loading,error} = useQuery(GET_ALL_POSTS);
-    // if (loading) {
-    //     return  <img src={spinner} alt="loading" /> 
-    // }
-    // if (data) {
-    //     console.log(data);
-    // }
-    // if (error) {
-    //     console.log(error);
-    // }
 
 
   return (
     
-    <div>
-      <h1 style={{color: "red"}}>WORKOUT POSTS TESTING!</h1>
-      <div style={{backgroundColor: "green", padding: " 20rem", margin: "auto"}}>
-        HI
-        </div>
-
-        {/* <form>
-            {data &&
-            data.getAllPosts.map((getAllPosts) => {
-                return (
-                    <form>
-                        <div>
-                        <label>{getAllPosts.username}</label>
-                        <textarea>{getAllPosts.createdAt}</textarea>
-                        </div>
-
-                    </form>
-                );
-        })}
-        </form>   */}
-
-      </div>
+    <Segment basic placeholder >
+      <video autoPlay loop muted>
+      <source src={video} type="video/mp4"/>
+     </video> 
+      </Segment>
 
    
 
