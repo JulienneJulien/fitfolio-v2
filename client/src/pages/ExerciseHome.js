cd8123db65b8b851cf0062430053739a5213283e
Your Name <JulienneJulien15@gmail.com>
2023-04-21 21:32:01 -0400

    Added Exercise components

--- client/src/pages/ExerciseHome.js
+++ client/src/pages/ExerciseHome.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import { Segment, Image, Header} from 'semantic-ui-react'
+import React, {useState} from 'react'
+import { Segment} from 'semantic-ui-react'
 import video from '../assets/welcomeVid.mp4'
 import '../styles/Exercise.css'
 function Exercise() {
 
 
   return (
-    
+   <Segment>
+    <HeroBanner/>
+    <SearchExercises/>
+    <Exercises/>
+    </Segment> 
+
+
+
     <Segment basic placeholder >
       <video autoPlay loop muted>
       <source src={video} type="video/mp4"/>
